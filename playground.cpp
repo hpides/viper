c641513fe001f6563f902c9e226f4f6bb235c286
Lawrence <github@lawben.com>
2020-04-17 18:22:12 +0200

    Add single-page get

--- playground.cpp
+++ playground.cpp
@@ -4,13 +4,14 @@ using namespace std;
 const uint64_t POOL_SIZE = 1024l * 1024 * 1024;
 
 int main() {
-
     viper::Viper<uint64_t, uint64_t> viper{"/mnt/nvrams1/viper.file", POOL_SIZE};
     cout << "viper size: " << sizeof(viper) << std::endl;
 //    cout << "viper map size: " << sizeof(viper.map_) << std::endl;
 //    cout << "viper pool size: " << sizeof(viper.v_pool_) << std::endl;
 
     viper.put(0, 0);
-    viper.put(1, 1);
-    viper.put(2, 2);
+    viper.put(1, 10);
+    viper.put(2, 20);
+
+    cout << viper.get(2) << endl;
 }
--- src/viper.hpp
+++ src/viper.hpp
@@ -18,14 +18,18 @@ static constexpr uint16_t NUM_SLOTS_PER_PAGE = 254;
 static constexpr uint32_t PAGE_SIZE = 4 * 1024; // 4kb
 static constexpr uint32_t NUM_DIMMS = 6;
 
-using SlotBitsetType = uint8_t;
+using offset_size_t = uint64_t;
+using block_size_t = uint64_t;
+using page_size_t = uint8_t;
+using slot_size_t = uint8_t;
 
 namespace internal {
 
 template <typename K, typename V>
 struct ViperPage {
+    using VEntry = std::pair<K, V>;
     std::bitset<NUM_SLOTS_PER_PAGE> free_slots;
-    std::array<std::pair<K, V>, NUM_SLOTS_PER_PAGE> data;
+    std::array<VEntry, NUM_SLOTS_PER_PAGE> data;
 
     ViperPage() {
         free_slots.flip();
@@ -65,12 +69,46 @@ struct ViperRoot {
     }
 };
 
+class KeyValueOffset {
+  public:
+    KeyValueOffset() : offset{0xFFFFFFFFFFFFFFFF} {}
+
+    KeyValueOffset(const block_size_t block_number, const page_size_t page_number, const slot_size_t slot)
+    : offset{shift_numbers(block_number, page_number, slot)} {}
+
+    inline std::tuple<block_size_t, page_size_t, slot_size_t> get_offsets() const {
+        return {get_block_number(), get_page_number(), get_slot_number()};
+    }
+
+    inline block_size_t get_block_number() const {
+        return offset & 0xFFFFFFFFFFFF0000u; // Bit 1 to 48
+    }
+
+    inline page_size_t get_page_number() const {
+        return (offset & 0xFF00u) >> 8u; // Bits 49 to 56
+    }
+
+    inline slot_size_t get_slot_number() const {
+        return offset & 0xFFu; // Bits 57 to 64
+    }
+
+  protected:
+    static offset_size_t shift_numbers(const block_size_t block_number, const page_size_t page_number, const slot_size_t slot) {
+        offset_size_t kv_offset = block_number << 16u;
+        kv_offset |= static_cast<offset_size_t>(page_number) << 8u;
+        kv_offset |= static_cast<offset_size_t>(slot);
+        return kv_offset;
+    }
+
+    offset_size_t offset;
+};
+
 } // namespace internal
 
 template <typename K, typename V>
 class Viper {
-    using DataOffsetType = const std::pair<K, V>*;
-    using MapType = tbb::concurrent_hash_map<K, DataOffsetType>;
+    using KVOffset = internal::KeyValueOffset;
+    using MapType = tbb::concurrent_hash_map<K, KVOffset>;
     using VPage = internal::ViperPage<K, V>;
     using VRoot = internal::ViperRoot<K, V>;
     using VPageBlock = typename VRoot::VPageBlock;
@@ -85,17 +123,26 @@ class Viper {
     bool remove(K key);
 
   protected:
-    VPage* get_free_v_page();
+    inline block_size_t get_next_block();
+    inline VPage* get_v_page(block_size_t block_number, page_size_t page_number);
 
 
     MapType map_;
     pobj::pool<VRoot> v_pool_;
     pobj::persistent_ptr<VRoot> v_root_;
+    std::vector<VPageBlock*> blocks_;
+
+    page_size_t num_pages_per_block_;
+    std::atomic<uint32_t> current_block_;
+    uint64_t current_page_;
+
+    std::atomic<uint64_t> current_size_;
 };
 
 template <typename K, typename V>
-Viper<K, V>::Viper(const std::string& pool_file, const uint64_t pool_size)
-    : map_(VRoot::VPageBlock::num_slots_per_block) {
+Viper<K, V>::Viper(const std::string& pool_file, const uint64_t pool_size) :
+    map_(VRoot::VPageBlock::num_slots_per_block),
+    current_block_(0), current_page_(0), current_size_(0) {
 
     int sds_write_value = 0;
     pmemobj_ctl_set(NULL, "sds.at_create", &sds_write_value);
@@ -113,6 +160,8 @@ Viper<K, V>::Viper(const std::string& pool_file, const uint64_t pool_size)
     }
 
     v_root_ = v_pool_.root();
+    blocks_.push_back(v_root_->v_page_blocks[0].get());
+    num_pages_per_block_ = VRoot::num_pages_per_block;
 }
 
 template <typename K, typename V>
@@ -123,34 +172,61 @@ Viper<K, V>::~Viper() {
 
 template <typename K, typename V>
 bool Viper<K, V>::put(K key, V value) {
-    VPage* v_page = get_free_v_page();
-    std::bitset<NUM_SLOTS_PER_PAGE>& free_slots = v_page->free_slots;
-    const size_t free_slot_idx = free_slots._Find_first();
+    block_size_t block_number;
+    page_size_t v_page_number;
+    VPage* v_page;
+    std::bitset<NUM_SLOTS_PER_PAGE> free_slots;
+    slot_size_t free_slot_idx;
 
-    if (free_slot_idx < free_slots.size()) {
-        v_page->data[free_slot_idx] = {key, value};
-        const std::pair<K, V>* entry_ptr = v_page->data.data() + free_slot_idx;
-        pmemobj_persist(v_pool_.handle(), entry_ptr, sizeof(K) + sizeof(V));
-        free_slots.flip(free_slot_idx);
-        pmemobj_persist(v_pool_.handle(), &(v_page->free_slots), sizeof(free_slots));
+    // Find free slot
+    do {
+        block_number = get_next_block();
+        v_page_number = ++current_page_ % num_pages_per_block_;
+        v_page = get_v_page(block_number, v_page_number);
+        free_slots = v_page->free_slots;
+        free_slot_idx = free_slots._Find_first();
+    } while (free_slot_idx == free_slots.size());
 
-        typename MapType::accessor accessor;
-        map_.insert(accessor, key);
-        accessor->second = entry_ptr;
-        return true;
-    } else {
-        // TODO: No free slot;
+    v_page->data[free_slot_idx] = {key, value};
+    const typename VPage::VEntry* entry_ptr = v_page->data.data() + free_slot_idx;
+    pmemobj_persist(v_pool_.handle(), entry_ptr, sizeof(typename VPage::VEntry));
+    free_slots.flip(free_slot_idx);
+    pmemobj_persist(v_pool_.handle(), &(v_page->free_slots), sizeof(free_slots));
+
+    const KVOffset kv_offset{block_number, v_page_number, free_slot_idx};
+    typename MapType::accessor accessor;
+    const bool is_new_item = map_.insert(accessor, {key, kv_offset});
+    if (!is_new_item) {
+        accessor->second = kv_offset;
     }
 
-    return false;
+    return true;
 }
 
 template <typename K, typename V>
-internal::ViperPage<K, V>* Viper<K, V>::get_free_v_page() {
-    VPageBlocks& v_blocks = v_root_->v_page_blocks;
-    VPageBlock& next_v_block = *(v_blocks[0]);
-    VPage* v_page = &(next_v_block.v_pages[0]);
-    return v_page;
+V Viper<K, V>::get(K key) {
+    typename MapType::const_accessor result;
+    const bool found = map_.find(result, key);
+    if (!found) {
+        throw std::runtime_error("Key '" + std::to_string(key) + "' not found.");
+    }
+
+    const KVOffset kv_offset = result->second;
+    const auto [block_number, page_number, slot_number] = kv_offset.get_offsets();
+    // TODO: check how this is optimized by compiler
+    return blocks_[block_number]->v_pages[page_number].data[slot_number].second;
+}
+
+template <typename K, typename V>
+block_size_t Viper<K, V>::get_next_block() {
+    return current_block_;
+}
+
+template <typename K, typename V>
+internal::ViperPage<K, V>* Viper<K, V>::get_v_page(const block_size_t block_number, const page_size_t page_number) {
+    // TODO: check how this is optimized by compiler
+    VPageBlock* next_v_block = blocks_[block_number];
+    return &(next_v_block->v_pages[page_number]);
 }
 
 }  // namespace viper
