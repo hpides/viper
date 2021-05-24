6f988135cf1fc29a5b3e6eeea774ff6e24082e30
Lawrence <github@lawben.com>
2021-05-24 22:20:44 +0200

    Update example

--- playground.cpp
+++ playground.cpp
@@ -3,8 +3,8 @@
 #include "viper/viper.hpp"
 
 int main(int argc, char** argv) {
-    const size_t inital_size = 1073741824;  // 1 GiB
-    auto viper_db = viper::Viper<uint64_t, uint64_t>::create("/mnt/pmem2/viper", inital_size);
+    const size_t initial_size = 1073741824;  // 1 GiB
+    auto viper_db = viper::Viper<uint64_t, uint64_t>::create("/mnt/pmem2/viper", initial_size);
 
     // To modify records in Viper, you need to use a Viper Client.
     auto v_client = viper_db->get_client();
@@ -14,9 +14,13 @@ int main(int argc, char** argv) {
         v_client.put(key, value);
     }
 
-    for (uint64_t key = 0; key < 10; ++key) {
+    for (uint64_t key = 0; key < 11; ++key) {
         uint64_t value;
-        v_client.get(key, &value);
-        std::cout << "Record: " << key << " --> " << value << std::endl;
+        const bool found = v_client.get(key, &value);
+        if (found) {
+            std::cout << "Record: " << key << " --> " << value << std::endl;
+        } else {
+            std::cout << "No record found for: " << key << std::endl;
+        }
     }
 }
