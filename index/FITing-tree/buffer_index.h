7dc1ff620e35f6e8e732d262ed9fbfe59d727daf
gejiake <568998464@qq.com>
2021-12-21 21:12:28 +0800

    update

--- index/FITing-tree/buffer_index.h
+++ index/FITing-tree/buffer_index.h
@@ -89,15 +89,27 @@ public:
     }
 
     // Point query
-    P find(K key) const {
+    P find(K key) {
         if (!less_than(key, min_key) && !greater_than(key, max_key)) {
-            return (--btree.upper_bound(key))->second->find(key);
+            this->LogHdr1Start();
+            auto * p1=(--btree.upper_bound(key))->second;
+            this->LogHdr1End();
+            this->LogHdr2Start();
+            auto res=p1->find(key);
+            this->LogHdr2End();
+            return res;
         }
         else if (less_than(key, min_key)) {
-            return left_buffer.find(key);
+            this->LogHdr2Start();
+            auto res=left_buffer.find(key);
+            this->LogHdr2End();
+            return res;
         }
         else {
-            return right_buffer.find(key);
+            this->LogHdr2Start();
+            auto res=right_buffer.find(key);
+            this->LogHdr2End();
+            return res;
         }
     }
 
